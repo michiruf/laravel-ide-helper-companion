f0520601955245220b1032c91da817658804776f
Michael Ruf <git@michiruf.de>
2024-08-17 21:13:57 +0200

    Use git to undo the user model change

--- tests/Feature/IdeHelperCompanionCommandTest.php
+++ tests/Feature/IdeHelperCompanionCommandTest.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\File;
+use Illuminate\Process\Factory as Process;
 
 it('can execute the command', function () {
-    File::delete('_ide_helper.php');
-    File::delete('_ide_helper_models.php');
-    File::delete('.phpstorm.meta.php');
-
+    // Setup: clear previous generated stuff
     File::deleteDirectory(config('ide-helper-companion.base_directory'));
 
+    // Action
     $this->artisan('ide-helper-companion')->assertOk();
 
+    // Assert: created files
     expect()
         ->and(File::exists('_ide_helper.php'))->toBeFalse()
         ->and(File::exists('_ide_helper_models.php'))->toBeFalse()
@@ -19,6 +19,21 @@ it('can execute the command', function () {
         ->and(File::exists(config('ide-helper-companion.base_directory').'/_ide_helper_models.php'))->toBeTrue()
         ->and(File::exists(config('ide-helper-companion.base_directory').'/.phpstorm.meta.php'))->toBeTrue();
 
+    // Assert: user model was annotated
     $userModelContent = file_get_contents(base_path('../../../../workbench/app/Models/User.php'));
     expect($userModelContent)->toContain('@mixin IdeHelperUser');
+
+    // Setup: revert the user model
+    $gitVersion = (new Process())->run('git --version');
+    expect($gitVersion)->successful()->toBeTrue($gitVersion->output());
+    $gitSafeDirectory =  (new Process())->run('git config --global --add safe.directory /var/www/html');
+    expect($gitSafeDirectory)->successful()->toBeTrue($gitSafeDirectory->output());
+    $gitRevertUserModel =  (new Process())->command('git checkout -- workbench/app/Models/User.php')
+        ->path(base_path('../../../../'))
+        ->run();
+    expect($gitRevertUserModel)->successful()->toBeTrue($gitRevertUserModel->output());
+
+    // Assert: user model does not contain the generated mixin annotation
+    $userModelContent = file_get_contents(base_path('../../../../workbench/app/Models/User.php'));
+    expect($userModelContent)->not->toContain('@mixin IdeHelperUser');
 });
