05c48ea2035a357cd993414f893ca8da50ead5c5
Michael Ruf <git@michiruf.de>
2024-09-02 17:16:07 +0200

    Remove docblock from test product model

--- workbench/app/Models/Product.php
+++ workbench/app/Models/Product.php
@@ -4,12 +4,6 @@ namespace Workbench\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @property int $id
- * @property string $name
- * @property \Illuminate\Support\Carbon $created_at
- * @property \Illuminate\Support\Carbon $updated_at
- */
 class Product extends Model
 {
     protected $guarded = [];
