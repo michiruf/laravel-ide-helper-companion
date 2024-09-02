b67a742e07957c2d73d364114fbb7bc6301cc017
Michael Ruf <git@michiruf.de>
2024-09-02 17:14:05 +0200

    Enhance annotate command by also executing pint if option specified

--- src/Commands/AnnotateCommand.php
+++ src/Commands/AnnotateCommand.php
@@ -3,12 +3,14 @@
 namespace IdeHelperCompanion\Commands;
 
 use IdeHelperCompanion\Commands\Helper\ThrottleCommandHelper;
+use IdeHelperCompanion\Data\ClassDefinition;
 use IdeHelperCompanion\Services\ModelDirectoryProcessor;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
 
 class AnnotateCommand extends Command
 {
-    public $signature = 'ide-helper-companion:annotate {--D|dir=} {--throttle=0}';
+    public $signature = 'ide-helper-companion:annotate {--D|dir=} {--throttle=0} {--pint}';
 
     public $description = 'Command to annotate model files';
 
@@ -17,7 +19,7 @@ class AnnotateCommand extends Command
         $throttle = (int) ($this->option('throttle') ?? 0);
 
         return ThrottleCommandHelper::mayExecuteThrottled(
-            'ide-helper-companion',
+            'ide-helper-companion:annotate',
             $throttle,
             fn () => $this->handleNow($processor)
         );
@@ -34,6 +36,13 @@ class AnnotateCommand extends Command
             ->loadClasses($dir)
             ->filterClasses()
             ->processModels();
+
+        if ($this->option('pint')) {
+            $classFilePaths = $processor->classes
+                ->map(fn (ClassDefinition $definition) => $definition->filePath)
+                ->join(' ');
+            Process::path(base_path())->run("vendor/bin/pint $classFilePaths");
+        }
 
         return self::SUCCESS;
     }
